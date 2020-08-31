d9d0776c4f733d3860dabc0e30dc44dce8ef8c09
Joachim Løvgaard <joachim@loevgaard.dk>
2020-08-31 10:42:31 +0200

    Updated tag bag dependency to use generic template tag

--- src/Tag/PhpTemplatesTag.php
+++ src/Tag/PhpTemplatesTag.php
@@ -4,38 +4,10 @@ declare(strict_types=1);
 
 namespace Setono\TagBag\Tag;
 
-class PhpTemplatesTag extends Tag implements PhpTemplatesTagInterface
+class PhpTemplatesTag extends TemplateTag implements PhpTemplatesTagInterface
 {
     /** @var string */
-    protected $template;
-
-    /** @var array */
-    protected $context;
-
-    public function __construct(string $template, array $context = [])
-    {
-        $this->template = $template;
-        $this->context = $context;
-
-        $this->setName('setono_tag_bag_php_templates_tag');
-    }
-
-    public function getTemplate(): string
-    {
-        return $this->template;
-    }
-
-    public function getContext(): array
-    {
-        return $this->context;
-    }
-
-    public function setContext(array $context): self
-    {
-        $this->context = $context;
-
-        return $this;
-    }
+    protected $name = 'setono_tag_bag_php_templates_tag';
 
     /**
      * @param mixed $value
--- src/Tag/PhpTemplatesTagInterface.php
+++ src/Tag/PhpTemplatesTagInterface.php
@@ -4,15 +4,6 @@ declare(strict_types=1);
 
 namespace Setono\TagBag\Tag;
 
-interface PhpTemplatesTagInterface extends TagInterface
+interface PhpTemplatesTagInterface extends TemplateTagInterface
 {
-    /**
-     * Returns the PHP template
-     */
-    public function getTemplate(): string;
-
-    /**
-     * Returns the context to inject into the php template when rendered
-     */
-    public function getContext(): array;
 }
